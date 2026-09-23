84f38bcb6b80054042f98307c98fc5b2ebe173a6
ip61-ua <ip61@alu.ua.es>
2026-09-23 22:51:50 +0200

    chore: copying from práctica 1 to práctica 2

--- practica1/src/ejercicio3.adb
+++ practica1/src/ejercicio3.adb
@@ -16,9 +16,9 @@ begin
    -- Aft => 1 es poner solo un decimal
    Ada.Float_Text_IO.Put
      (Item => pkg_ejercicio2.obtenerNota, Aft => 1, Exp => 0);
-	  
+
    Ada.Text_Io.New_Line;
-		
+
    -- 4.b) No funca porque es de otro paquete. Falta use o espacio de nombres.
    --   Put no es visible! No sabemos a cual nos referimos.
 
@@ -28,17 +28,20 @@ begin
       Ada.Text_IO.New_Line;
    end loop;
 
-   Ada.Text_Io.Put_Line ("Introduzca por teclado un día cualquiera: ");	
-   Dias_IO.Get(Item => e);
-		
+   Ada.Text_Io.Put_Line ("Introduzca por teclado un día cualquiera: ");
+   Dias_IO.Get (Item => e);
+
    -- 4.e)	 Peta!
-   
+
    case e is
-      when pkg_ejercicio2.Lunes => Ada.Text_IO.Put_Line("El Lunes hay clases de STR");
-      when others => Ada.Text_IO.Put("El ");
-         Dias_IO.Put(Item => e);
-         Ada.Text_IO.Put_Line(" no hay clases de STR");
-   end case;										    
-	-----------------------------------------------------------------------------------------------
-						
+      when pkg_ejercicio2.Lunes =>
+         Ada.Text_IO.Put_Line ("El Lunes hay clases de STR");
+
+      when others               =>
+         Ada.Text_IO.Put ("El ");
+         Dias_IO.Put (Item => e);
+         Ada.Text_IO.Put_Line (" no hay clases de STR");
+   end case;
+   -----------------------------------------------------------------------------------------------
+
 end ejercicio3;
