1cc2737e1c40736ac1b2df930b5926ebaff3ddef
ip61-ua <ip61@alu.ua.es>
2026-09-24 10:51:07 +0200

    feat: terminar práctica 2

--- practica2/src/main.adb
+++ practica2/src/main.adb
@@ -1,6 +1,6 @@
 with Ada.Integer_Text_IO;  USE Ada.Integer_Text_IO;
 with Ada.Text_Io;          use Ada.Text_Io;
-with pkg_graficos;
+with pkg_graficos;         use pkg_graficos;
 with pkg_tipos;            use pkg_tipos;
 with Ada.Numerics.Discrete_Random;
 with Pkg_tareas_dinamicas; use Pkg_tareas_dinamicas;
@@ -12,34 +12,46 @@ procedure main is
    Retardo     : T_Retardoaparicionaviones;
    tarea_avion : Ptr_Tarea_Avion;
    ptr_avion   : Ptr_T_RecordAvion;
+
+   task Window_Simulator;
+   task body Window_Simulator is
+   begin
+      Pkg_Graficos.Simular_Sistema;
+   end;
+
 begin
-   -- Pkg_Graficos.Simular_Sistema;
    Pkg_Custom_Aleatorio.Reset (Generador);
+   for id in T_IdAvion loop
+      for aerovia in T_Rango_AereoVia'First .. 4 loop
+         -- T_Rango_AereoVia'Last loop
 
-   loop
-      for id in T_IdAvion loop
-         for aerovia in T_Rango_AereoVia'First .. T_Rango_AereoVia'Last loop
+         -- inicializar datos de un nuevo avion
+         ptr_avion := new T_RecordAvion;
 
-            -- inicializar datos de un nuevo avion
-            ptr_avion := new T_RecordAvion;
+         ptr_avion.id := id;
+         if (Aerovia mod 2) = 0 then
+            ptr_avion.velocidad.x := -T_Rangovelocidad'last;
+         else
+            ptr_avion.velocidad.x := T_Rangovelocidad'last;
+         end if;
+         Ptr_Avion.Pos := Pos_Inicio (aerovia);
+         ptr_avion.velocidad.y := 0;
+         ptr_avion.tren_aterrizaje := False;
+         ptr_avion.aereovia := aerovia;
+         ptr_avion.aereovia_inicial := aerovia;
+         ptr_avion.color := blue;
+         ptr_avion.pista := SIN_PISTA;
 
-            ptr_avion.id := id;
-            ptr_avion.velocidad.x := VELOCIDAD_VUELO;
-            ptr_avion.velocidad.y := 0;
-            ptr_avion.tren_aterrizaje := False;
-            ptr_avion.aereovia := aerovia;
+         -- Crear una tarea para el comportamiento del avion
+         tarea_avion := new T_Tarea_Avion (ptr_avion);
 
-            -- Crear una tarea para el comportamiento del avion
-            tarea_avion := new T_Tarea_Avion (ptr_avion);
+         Retardo := Pkg_Custom_Aleatorio.Random (Generador);
+         Put ("Esperando ");
+         Put (Item => Retardo, Width => 0);
+         Put (" segundos...");
+         New_Line;
 
-            Retardo := Pkg_Custom_Aleatorio.Random (Generador);
-            Put ("Esperando ");
-            Put (Item => Retardo, Width => 0);
-            Put (" segundos...");
-            New_Line;
-
-            delay (Duration (Retardo));
-         end loop;
+         delay (Duration (Retardo));
       end loop;
    end loop;
 end main;
--- practica2/src/pkg_tareas_dinamicas.adb
+++ practica2/src/pkg_tareas_dinamicas.adb
@@ -1,14 +1,59 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;    use Ada.Text_IO;
+with Ada.Exceptions; use Ada.Exceptions;
+with PKG_debug;      use PKG_debug;
+with PKG_graficos;   use PKG_graficos;
+with PKG_tipos;      use PKG_tipos;
 
 package body Pkg_tareas_dinamicas is
 
    task body T_Tarea_Avion is
    begin
       Put_line
-        ("TASK Avion: "
+        ("TASK Avion{"
+         & " id ="
          & T_IdAvion'Image (ptr_avion.id)
-         & " -"
-         & T_Rango_AereoVia'Image (ptr_avion.aereovia));
+         & " pos = ("
+         & T_Coordenadax'image (ptr_avion.Pos.X)
+         & ","
+         & T_Coordenaday'image (ptr_avion.Pos.y)
+         & ") "
+         & " velocidad =("
+         & T_RangoVelocidad'image (ptr_avion.velocidad.X)
+         & ","
+         & T_RangoVelocidad'image (ptr_avion.velocidad.y)
+         & ") "
+         & " color ="
+         & T_Color'Image (ptr_avion.color)
+         & " aereovia ="
+         & T_Rango_Aereovia'Image (ptr_avion.aereovia)
+         & " aereovia_inicial ="
+         & T_Rango_Aereovia'Image (Ptr_Avion.aereovia_inicial)
+         & " pista ="
+         & T_PistaAterrizaje'Image (Ptr_Avion.pista)
+         & " tren_aterrizaje ="
+         & Boolean'Image (Ptr_Avion.tren_aterrizaje)
+         & " }");
+
+      Aparece (Ptr_Avion.all);
+      loop
+         Actualiza_Movimiento (Ptr_Avion.all);
+         delay (RETARDO_MOVIMIENTO);
+      end loop;
+
+   exception
+      when Detectada_Colision =>
+         PKG_debug.Escribir
+           ("El avión con id "
+            & T_IdAvion'Image (ptr_avion.id)
+            & " no puede despegar por colisión");
+         Desaparece (Ptr_Avion.all);
+      when event : others =>
+         PKG_debug.Escribir
+           ("ERROR en TASK ….: "
+            & Exception_Name (Exception_Identity (event))
+            & " "
+            & Exception_Message (event));
+
    end T_Tarea_Avion;
 
 end pkg_tareas_dinamicas;
